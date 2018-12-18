9e194c874e7cff390e8666c9aebc2d717e00f9b1
student <student@imac-15.local>
2018-12-18 12:47:38 +0800

    Initial Commit

---
+++ bxbNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bxbNetwork
+//
+//  Created by student on 2018/12/18.
+//  Copyright © 2018年 baoxinbei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
